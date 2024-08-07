2085a1f2eebccb5803151465eaccac0c8cfb332d
OriQuack <kimdoh0423@naver.com>
2024-08-07 21:17:35 +0900

    .

--- Authorization/controllers/auth.js
+++ Authorization/controllers/auth.js
@@ -1,344 +1,157 @@
 const bcrypt = require('bcryptjs');
 const { OAuth2Client } = require('google-auth-library');
+const axios = require('axios');
+const generator = require('generate-password');
 
 const User = require('../models/user');
 const generateToken = require('../util/token');
 
+const sendErrorResponse = (res, statusCode, message) => {
+    return res.status(statusCode).json({ message });
+};
+
+const generateAuthResponse = (res, statusCode, accessToken, refreshToken, user) => {
+    return res
+        .status(statusCode)
+        .cookie('refreshToken', refreshToken, {
+            expires: new Date(Date.now() + 259200000),
+            httpOnly: true,
+        })
+        .header('Authorization', accessToken)
+        .json(user);
+};
 
 exports.postLogin = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    User.getUserByEmail(email).then((user) => {
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        bcrypt
-            .compare(password, user.password)
-            .then((doMatch) => {
-                if (doMatch) {
+    const { email, password } = req.body;
+
+    User.getUserByEmail(email)
+        .then((user) => {
+            if (!user) {
+                return sendErrorResponse(res, 404, 'User not found');
+            }
+
+            return bcrypt.compare(password, user.password)
+                .then((doMatch) => {
+                    if (!doMatch) {
+                        return sendErrorResponse(res, 401, 'Incorrect password');
+                    }
+
                     const accessToken = generateToken.genAccessToken(user.email);
                     const refreshToken = generateToken.genRefreshToken();
-                    return res
-                        .status(200)
-                        .cookie('refreshToken', refreshToken, {
-                            expires: new Date(Date.now() + 259200000),
-                            httpOnly: true,
-                        })
-                        .header('Authorization', accessToken)
-                        .json({ user_id: user.user_id, username: user.username });
-                }
-                return res.status(401).json({ message: 'Incorrect password' });
-            });
-    }).catch((err) => {
-        return res.status(500).json({ message: 'Server error' });
-    });
+                    return generateAuthResponse(res, 200, accessToken, refreshToken, { user_id: user.user_id, username: user.username });
+                });
+        })
+        .catch(() => sendErrorResponse(res, 500, 'Server error'));
 };
 
 exports.postSignup = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const username = req.body.username;
+    const { email, password, username } = req.body;
+
     User.getUserByEmail(email)
         .then((user) => {
             if (user) {
-                return res.status(400).json({ message: 'User already exists' });
+                return sendErrorResponse(res, 400, 'User already exists');
             }
-        })
-        .catch((err) => {
-            return res.status(500).json({ message: 'Server error' });
-        });
-    bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-            const user = new User({
-                username: username,
-                email: email,
-                password: hashedPassword,
-            });
-            return user.save();
-        })
-        .then((result) => {
-            const accessToken = generateToken.genAccessToken(email);
-            const refreshToken = generateToken.genRefreshToken();
-            return res
-                .status(201)
-                .cookie('refreshToken', refreshToken, {
-                    expires: new Date(Date.now() + 259200000),
-                    httpOnly: true,
+
+            return bcrypt.hash(password, 12)
+                .then((hashedPassword) => {
+                    const newUser = new User({ username, email, password: hashedPassword });
+                    return newUser.save();
                 })
-                .header('Authorization', accessToken)
-                .json({ email: email, username: username });
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json({ message: 'Server error' });
-        });
-};
-
-exports.postGoogleLogin = async (req, res) => {
-    const client = new OAuth2Client();
-    const CLIENT_ID = '';
-    const email = req.body.email;
-    const token = req.body.access_token;
-
-    // verify token and email
-    try {
-        const ticket = await client
-            .verifyIdToken({
-                idToken: token,
-                audience: CLIENT_ID,
-            });
-
-        const payload = ticket.getPayload();
-        const tokenEmail = payload['email'];
-        if (email != tokenEmail) {
-            return res.status(400).json({ message: 'Bad request' });
-        }
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: 'Google API server error' });
-    }
-
-    User.getUserByEmail(email).then((user) => {
-        if (!user) {
-            // signup
-            const username = '여행자' +
-                generator.generate({
-                    length: 8,
-                    numbers: true,
-                });
-            const password = generator.generate({
-                length: 14,
-                numbers: true,
-                symbols: true,
-                strict: true,
-            });
-            bcrypt.hash(password, 12).then((hashedPassword) => {
-                const newUser = new User({
-                    username: username,
-                    email: email,
-                    password: hashedPassword,
-                });
-                newUser
-                    .save()
-                    .then((result) => {
-                        const accessToken =
-                            token.genAccessToken(email);
-                        const refreshToken = token.genRefreshToken();
-                        return res
-                            .status(201)
-                            .cookie('refreshToken', refreshToken, {
-                                expires: new Date(Date.now() + 259200000),
-                                httpOnly: true,
-                            })
-                            .header('Authorization', accessToken)
-                            // TODO: user_id
-                            .json({ user_id: result.user_id, username: username });
-                    });
-            })
-                .catch((err) => {
-                    console.log(err);
-                    return res
-                        .status(500)
-                        .json({ message: 'Server error' });
-                });
-        } else {
-            // login
-            const accessToken = generateToken.genAccessToken(user.email);
-            const refreshToken = generateToken.genRefreshToken();
-            return res
-                .status(200)
-                .cookie('refreshToken', refreshToken, {
-                    expires: new Date(Date.now() + 259200000),
-                    httpOnly: true,
-                })
-                .header('Authorization', accessToken)
-                .json({ user_id: user.user_id, username: user.username });
-        }
-    }).catch((err) => {
-        return res.status(500).json({ message: 'Server error' });
-    });
-};
-
-
-exports.postKakaoAuth = async (req, res, next) => {
-    const email = req.body.email;
-    const token = req.body.access_token;
-
-    // verify token and email
-    try {
-        const userInfoResponse = await axios.get('https://kapi.kakao.com/v2/user/me', {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        const tokenEmail = userInfoResponse.data.kakao_account.email;
-        if (email != tokenEmail) {
-            return res.status(400).json({ message: 'Bad request' });
-        }
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: 'Naver API Server error' });
-    }
-
-    User.getUserByEmail(email).then((user) => {
-        if (!user) {
-            // signup
-            const username = '여행자' +
-                generator.generate({
-                    length: 8,
-                    numbers: true,
-                });
-            const password = generator.generate({
-                length: 14,
-                numbers: true,
-                symbols: true,
-                strict: true,
-            });
-            bcrypt.hash(password, 12).then((hashedPassword) => {
-                const newUser = new User({
-                    username: username,
-                    email: email,
-                    password: hashedPassword,
-                });
-                newUser
-                    .save()
-                    .then((result) => {
-                        const accessToken = generateToken.genAccessToken(email);
-                        const refreshToken = generateToken.genRefreshToken();
-                        return res
-                            .status(201)
-                            .cookie('refreshToken', refreshToken, {
-                                expires: new Date(Date.now() + 259200000),
-                                httpOnly: true,
-                            })
-                            .header('Authorization', accessToken)
-                            // TODO
-                            .json({ user_id: result.user_id, username: username });
-                    });
-            })
-                .catch((err) => {
-                    console.log(err);
-                    return res.status(500).json({ message: 'Server error' });
-                });
-        } else {
-            // login
-            const accessToken = generateToken.genAccessToken(user.email);
-            const refreshToken = generateToken.genRefreshToken();
-            return res
-                .status(200)
-                .cookie('refreshToken', refreshToken, {
-                    expires: new Date(Date.now() + 259200000),
-                    httpOnly: true,
-                })
-                .header('Authorization', accessToken)
-                .json({ user_id: user.user_id, username: user.username });
-        }
-    }).catch((err) => {
-        return res.status(500).json({ message: 'Server error' });
-    });
-};
-
-exports.postNaverLogin = async (req, res, next) => {
-    const email = req.body.email;
-    const token = req.body.access_token;
-
-    try {
-        const info = await axios.get('https://openapi.naver.com/v1/nid/me', {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-        const tokenEmail = info.data.response.email;
-
-        if (email != tokenEmail) {
-            return res.status(400).json({ message: 'Bad request' });
-        }
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: 'Naver API Server error' });
-    }
-
-    User.getUserByEmail(email).then((user) => {
-        if (!user) {
-            // signup
-            const username = '여행자' +
-                generator.generate({
-                    length: 8,
-                    numbers: true,
-                });
-            const password = generator.generate({
-                length: 14,
-                numbers: true,
-                symbols: true,
-                strict: true,
-            });
-            bcrypt.hash(password, 12).then((hashedPassword) => {
-                const newUser = new User({
-                    username: username,
-                    email: email,
-                    password: hashedPassword,
-                });
-                newUser.save().then((result) => {
+                .then(() => {
                     const accessToken = generateToken.genAccessToken(email);
                     const refreshToken = generateToken.genRefreshToken();
-                    return res
-                        .status(201)
-                        .cookie('refreshToken', refreshToken, {
-                            expires: new Date(Date.now() + 259200000),
-                            httpOnly: true,
-                        })
-                        .header('Authorization', accessToken)
-                        // TODO
-                        .json({ user_id: result.user_id, username: username });
+                    return generateAuthResponse(res, 201, accessToken, refreshToken, { email, username });
                 });
-            });
-        } else {
-            // login
-            const accessToken = generateToken.genAccessToken(user.email);
-            const refreshToken = generateToken.genRefreshToken();
-            return res
-                .status(200)
-                .cookie('refreshToken', refreshToken, {
-                    expires: new Date(Date.now() + 259200000),
-                    httpOnly: true,
-                })
-                .header('Authorization', accessToken)
-                .json({ user_id: user.user_id, username: user.username });
-        }
-    }).catch((err) => {
-        return res.status(500).json({ message: 'Server error' });
-    });
+        })
+        .catch(() => sendErrorResponse(res, 500, 'Server error'));
 };
 
 exports.postUpdateProfile = (req, res, next) => {
-    const username = req.body.username;
-    const email = req.body.email;
-    const status_message = req.body.status_message;
-    const new_password = req.body.new_password;
+    const { username, email, status_message, new_password } = req.body;
 
-    User.getUserByEmail(email).then((user) => {
-        bcrypt.hash(new_password, 12).then((hashedPassword) => {
-            user.updateProfile(
-                username = username,
-                status_message = status_message,
-                hashedPassword = hashedPassword,
-            ).then(() => {
-                return res.status(200).json({ message: 'Success' });
-            });
-        });
-    }).catch((err) => {
-        return res.status(500).json({ message: 'Server error' });
-    });
+    User.getUserByEmail(email)
+        .then((user) => {
+            return bcrypt.hash(new_password, 12)
+                .then((hashedPassword) => user.updateProfile({ username, status_message, password: hashedPassword }))
+                .then(() => res.status(200).json({ message: 'Success' }));
+        })
+        .catch(() => sendErrorResponse(res, 500, 'Server error'));
 };
 
 exports.deleteUserData = (req, res, next) => {
-    req.user
-        .deleteUser()
-        .then((result) => {
-            return res.status(200).json({ message: 'Success signout' });
+    req.user.deleteUser()
+        .then(() => res.status(200).json({ message: 'Success signout' }))
+        .catch(() => sendErrorResponse(res, 500, 'Server error'));
+};
+
+const handleSocialLogin = (req, res, tokenVerifier, tokenName) => {
+    const { email, access_token } = req.body;
+
+    tokenVerifier(access_token)
+        .then((tokenEmail) => {
+            if (email !== tokenEmail) {
+                return sendErrorResponse(res, 400, 'Bad request');
+            }
+
+            return User.getUserByEmail(email)
+                .then((user) => {
+                    if (!user) {
+                        const username = '여행자' + generator.generate({ length: 8, numbers: true });
+                        const password = generator.generate({ length: 14, numbers: true, symbols: true, strict: true });
+
+                        return bcrypt.hash(password, 12)
+                            .then((hashedPassword) => {
+                                const newUser = new User({ username, email, password: hashedPassword });
+                                return newUser.save()
+                                    .then((result) => {
+                                        const accessToken = generateToken.genAccessToken(email);
+                                        const refreshToken = generateToken.genRefreshToken();
+                                        return generateAuthResponse(res, 201, accessToken, refreshToken, { user_id: result.user_id, username });
+                                    });
+                            });
+                    }
+
+                    const accessToken = generateToken.genAccessToken(user.email);
+                    const refreshToken = generateToken.genRefreshToken();
+                    return generateAuthResponse(res, 200, accessToken, refreshToken, { user_id: user.user_id, username: user.username });
+                });
         })
         .catch((err) => {
-            return res.status(500).json({ message: 'Server error' });
+            console.log(err);
+            return sendErrorResponse(res, 500, `${tokenName} API server error`);
         });
 };
+
+exports.postGoogleLogin = (req, res) => {
+    const CLIENT_ID = '';
+    const client = new OAuth2Client(CLIENT_ID);
+
+    const verifyGoogleToken = (token) => {
+        return client.verifyIdToken({ idToken: token, audience: CLIENT_ID })
+            .then((ticket) => ticket.getPayload().email);
+    };
+
+    handleSocialLogin(req, res, verifyGoogleToken, 'Google');
+};
+
+exports.postKakaoAuth = (req, res, next) => {
+    const verifyKakaoToken = (token) => {
+        return axios.get('https://kapi.kakao.com/v2/user/me', {
+            headers: { Authorization: `Bearer ${token}` },
+        }).then((response) => response.data.kakao_account.email);
+    };
+
+    handleSocialLogin(req, res, verifyKakaoToken, 'Kakao');
+};
+
+exports.postNaverLogin = (req, res, next) => {
+    const verifyNaverToken = (token) => {
+        return axios.get('https://openapi.naver.com/v1/nid/me', {
+            headers: { Authorization: `Bearer ${token}` },
+        }).then((response) => response.data.response.email);
+    };
+
+    handleSocialLogin(req, res, verifyNaverToken, 'Naver');
+};
+
