f1c78a7d575293f9669ef09cd4ce87718c8a5446
OriQuack <kimdoh0423@naver.com>
2024-08-10 14:22:34 +0900

    modified model

--- Authorization/models/user.js
+++ Authorization/models/user.js
@@ -17,29 +17,19 @@ class User {
 
     // return 값으로 unique 한 user_id 받아와야 함
     save() {
-        const db = getDb();
-        if (this._id) {
-            // User exists -> update user
-            return db.collection('users').updateOne({ _id: this._id }, { $set: this });
-        }
-        return db.collection('users').insertOne(this);
+        //
     }
 
     updateProfile(username, status_message, hashedPassword) {
-        this.username = username;
-        this.status_message = status_message;
-        this.password = hashedPassword;
-        return this.save();
+        //
     }
 
     deleteUser() {
-        const db = getDb();
-        return db.collection('users').deleteOne({ _id: new mongodb.ObjectId(this._id) });
+        //
     }
 
     static getUserByEmail(userEmail) {
-        const db = getDb();
-        return db.collection('users').findOne({ email: userEmail });
+        //
     }
 }
 
