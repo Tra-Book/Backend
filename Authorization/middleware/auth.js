74f8189d4988e597ae60a85db9e11d708c0073f4
oriquack <kimdoh0423@naver.com>
2024-09-07 21:15:10 +0900

    removed RT

--- Authorization/middleware/auth.js
+++ Authorization/middleware/auth.js
@@ -1,13 +1,11 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const token = require('../utils/tokenUtil');
 
 const authenticate = async (req, res, next) => {
     const accessToken = req.headers['authorization'];
-    const refreshToken = req.cookies['refreshToken'];
 
-    if (!accessToken || !refreshToken) {
-        return res.status(403).json({ message: 'Authentication required: no AT or RT' });
+    if (!accessToken) {
+        return res.status(403).json({ message: 'Authentication required: no access token' });
     }
 
     try {
@@ -23,58 +21,13 @@ const authenticate = async (req, res, next) => {
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
+
         req.user = new User(user);
         return next();
     } catch (err) {
-        // AT invalid or expired
-        try {
-            const accessDecoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, {
-                complete: true,
-                algorithms: ['HS256'],
-                clockTolerance: 0,
-                ignoreExpiration: true,
-                ignoreNotBefore: false,
-            });
-
-            try {
-                jwt.verify(
-                    refreshToken,
-                    process.env.REFRESH_TOKEN_SECRET,
-                    {
-                        complete: true,
-                        algorithms: ['HS256'],
-                        clockTolerance: 0,
-                        ignoreExpiration: false,
-                        ignoreNotBefore: false,
-                    },
-                    async (err, refreshDecoded) => {
-                        if (err) {
-                            return res.status(403).json({
-                                message:
-                                    'Authentication required: AT expired and RT invalid or expired',
-                            });
-                        }
-
-                        // AT expired && RT valid -> AT 재발급
-                        const newAccessToken = token.genAccessToken(accessDecoded.payload.userId);
-                        return res
-                            .status(403)
-                            .cookie('refreshToken', refreshToken, {
-                                expires: new Date(Date.now() + 259200000),
-                                httpOnly: true,
-                            })
-                            .header('Authorization', newAccessToken)
-                            .json({ message: 'Renewed expired access token' });
-                    }
-                );
-            } catch (err) {
-                return res.status(403).json({
-                    message: 'Authentication required: AT expired and RT invalid or expired',
-                });
-            }
-        } catch (err) {
-            return res.status(403).json({ message: 'Authentication required: AT invalid' });
-        }
+        return res
+            .status(403)
+            .json({ message: 'Authentication required: invalid or expired access token' });
     }
 };
 
--- Authorization/utils/tokenUtil.js
+++ Authorization/utils/tokenUtil.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const genAccessToken = (userId) => {
     const payload = { userId: userId };
     return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '30m',
+        expiresIn: '1h',
         notBefore: '0',
         algorithm: 'HS256',
         // issuer
