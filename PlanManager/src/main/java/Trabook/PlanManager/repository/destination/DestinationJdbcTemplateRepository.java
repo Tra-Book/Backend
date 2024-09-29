1f766e853cbaf0cedb0056687c9c05ba5aee511f
Suyeong00 <hsy747@g.skku.edu>
2024-09-30 01:26:22 +0900

    fix : places/general에 numOfAdded 수정, numOfReview 추가 plans/general에 정렬기준 startDate, numOfComment 추가

--- PlanManager/src/main/java/Trabook/PlanManager/repository/destination/DestinationJdbcTemplateRepository.java
+++ PlanManager/src/main/java/Trabook/PlanManager/repository/destination/DestinationJdbcTemplateRepository.java
@@ -79,69 +79,6 @@ public class    DestinationJdbcTemplateRepository implements DestinationReposito
         return result;
     }
 
-    /*
-    ### 중요 ###
-    review 추가되면 review 랑 조인 + ORDER BY review DESC 필요
-     */
-    @Override
-    public List<PlaceForModalDTO> findCustomPlaceList(String search, List<String> state, List<String> category,
-                                           String sorts, Integer userId,
-                                           Boolean userScrapOnly) {
-        String likeSearch = "%" + search + "%"; // SQL injection 방지
-        List<Object> params = new ArrayList<>();
-
-        // 기본 쿼리
-        String sql = "SELECT Place.*, ST_X(coordinate) AS x, ST_Y(coordinate) AS y, " +
-                "CASE WHEN sp.placeId IS NOT NULL THEN TRUE ELSE FALSE END AS isScrapped," +
-                "pc.commentId, pc.content, pc.date  " +  // scrap 여부 확인
-                "FROM Place " +
-                "JOIN City ON Place.cityId = City.cityId " +
-                "LEFT JOIN PlaceComment pc ON pc.placeId = Place.placeId " +
-                "LEFT JOIN ScrappedPlace sp ON Place.placeId = sp.placeId AND sp.userId = ? " +  // ScrappedPlace를 LEFT JOIN
-                "WHERE (Place.placeName LIKE ? OR Place.description LIKE ?) ";
-
-        params.add(userId);
-        params.add(likeSearch);  // placeName LIKE ?
-        params.add(likeSearch);  // description LIKE ?
-
-        if(userScrapOnly) {
-            sql += "AND (sp.userId = ?) ";
-            params.add(userId);
-        }
-
-        // state 리스트가 비어있지 않으면 IN 절 추가
-        if (state != null && !state.isEmpty()) {
-            if (!state.get(0).equals("전체")) {
-                String statePlaceholders = String.join(", ", Collections.nCopies(state.size(), "?"));
-                sql += ("AND City.stateName IN (" + statePlaceholders + ") ");
-                params.addAll(state);
-            }
-        }
-
-        // subcategory 리스트가 비어있지 않으면 IN 절 추가
-        if (category != null && !category.isEmpty()) {
-            if (!category.get(0).equals("전체")) {
-                String subcategoryPlaceholders = String.join(", ", Collections.nCopies(category.size(), "?"));
-                sql += ("AND Place.category IN (" + subcategoryPlaceholders + ") ");
-                params.addAll(category);
-            }
-        }
-
-        // 정렬 조건 처리
-        sql += "ORDER BY " +
-                "CASE WHEN ? = 'numOfAdded' THEN Place.numOfAdded " +
-                "WHEN ? = 'star' THEN Place.star " +
-                "ELSE Place.star END DESC ";
-//                "date DESC ";
-
-
-        // 파라미터 추가
-        params.add(sorts);  // 첫 번째 CASE 조건 (numOfAdded, star)
-        params.add(sorts);  // 두 번째 CASE 조건 (numOfAdded, star)
-        return jdbcTemplate.query(sql, generalPlaceRowMapper(), params.toArray());
-    }
-
-
     @Override
     public void addPlaceLike(long userId, long placeId) {
         String sql = "INSERT INTO LikedPlace(userId,placeId)" +
@@ -206,6 +143,72 @@ public class    DestinationJdbcTemplateRepository implements DestinationReposito
         return jdbcTemplate.queryForObject(sql, new Object[]{placeId, userId}, Boolean.class);
 
     }
+
+    /*
+### 중요 ###
+review 추가되면 review 랑 조인 + ORDER BY review DESC 필요
+ */
+
+    //////////////// findCustomPlaceList 위치 내릴게 합칠때 위로 올리지마 ////////////////////////
+    @Override
+    public List<PlaceForModalDTO> findCustomPlaceList(String search, List<String> state, List<String> category,
+                                                      String sorts, Integer userId,
+                                                      Boolean userScrapOnly) {
+        String likeSearch = "%" + search + "%"; // SQL injection 방지
+        List<Object> params = new ArrayList<>();
+
+        // 기본 쿼리
+        String sql = "SELECT Place.*, ST_X(coordinate) AS x, ST_Y(coordinate) AS y, " +
+                "CASE WHEN sp.placeId IS NOT NULL THEN TRUE ELSE FALSE END AS isScrapped," +
+                "pc.commentId, pc.content, pc.date " +  // scrap 여부 확인
+                "FROM Place " +
+                "JOIN City ON Place.cityId = City.cityId " +
+                "LEFT JOIN PlaceComment pc ON pc.placeId = Place.placeId " +
+                "LEFT JOIN ScrappedPlace sp ON Place.placeId = sp.placeId AND sp.userId = ? " +  // ScrappedPlace를 LEFT JOIN
+                "WHERE (Place.placeName LIKE ? OR Place.description LIKE ?) ";
+
+        params.add(userId);
+        params.add(likeSearch);  // placeName LIKE ?
+        params.add(likeSearch);  // description LIKE ?
+
+        if(userScrapOnly) {
+            sql += "AND (sp.userId = ?) ";
+            params.add(userId);
+        }
+
+        // state 리스트가 비어있지 않으면 IN 절 추가
+        if (state != null && !state.isEmpty()) {
+            if (!state.get(0).equals("전체")) {
+                String statePlaceholders = String.join(", ", Collections.nCopies(state.size(), "?"));
+                sql += ("AND City.stateName IN (" + statePlaceholders + ") ");
+                params.addAll(state);
+            }
+        }
+
+        // subcategory 리스트가 비어있지 않으면 IN 절 추가
+        if (category != null && !category.isEmpty()) {
+            if (!category.get(0).equals("전체")) {
+                String subcategoryPlaceholders = String.join(", ", Collections.nCopies(category.size(), "?"));
+                sql += ("AND Place.category IN (" + subcategoryPlaceholders + ") ");
+                params.addAll(category);
+            }
+        }
+
+        // 정렬 조건 처리
+
+        sql += "ORDER BY " +
+                "CASE WHEN ? = 'numOfAdded' THEN Place.numOfAdded " +
+                "WHEN ? = 'star' THEN Place.star " +
+                "ELSE Place.star END DESC ";
+//                "date DESC ";
+//
+//
+//        // 파라미터 추가
+        params.add(sorts);  // 첫 번째 CASE 조건 (numOfAdded, star)
+        params.add(sorts);  // 두 번째 CASE 조건 (numOfAdded, star)
+        return jdbcTemplate.query(sql, generalPlaceRowMapper(), params.toArray());
+    }
+
     private RowMapper<PlaceForModalDTO> generalPlaceRowMapper() {
         return new RowMapper<PlaceForModalDTO>() {
             @Override
@@ -213,7 +216,7 @@ public class    DestinationJdbcTemplateRepository implements DestinationReposito
                 Place place = new Place();
                 place.setPlaceName(rs.getString("placeName"));
                 place.setPlaceId(rs.getLong("placeId"));
-                place.setNumOfAdded(rs.getInt("scraps"));
+                place.setNumOfAdded(rs.getInt("numOfAdded"));
                 place.setAddress(rs.getString("address"));
                 place.setLongitude(rs.getDouble("x"));
                 place.setLatitude(rs.getDouble("y"));
@@ -228,7 +231,7 @@ public class    DestinationJdbcTemplateRepository implements DestinationReposito
                 place.setScraps(rs.getInt("scraps"));
                 //place.setZipcode(rs.getString("zipcode"));
                 place.setDescription(rs.getString("description"));
-
+                place.setNumOfReview(rs.getInt("numOfReview"));
 
                 List<PlaceComment> comments = new ArrayList<>();
                 int commentCount = 0;
